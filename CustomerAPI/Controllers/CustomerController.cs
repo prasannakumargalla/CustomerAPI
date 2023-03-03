6693b0d66d61b143918280d89fa0fd24ca4b26dd
prasannakumargalla <galla.prasanna86@gmail.com>
2023-03-02 21:35:05 -0500

    Add project files.

---
+++ CustomerAPI/Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CustomerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+
+        public CustomerController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [HttpGet(Name = "GetCustomer")]
+        public IEnumerable<clsCustomer> Get()
+        {
+            List<clsCustomer> liCustomer = new List<clsCustomer>();
+            liCustomer.Add(new clsCustomer() { title = "Prasanna", description = "Galla" });
+            return liCustomer.AsEnumerable();
+
+        }
+
+        [HttpPost]
+        public void PostTodoItem(clsCustomer todoDTO)
+        {
+
+            var connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+
+            //string connectionString = @"Data Source=PRASANNA;Initial Catalog=EcommerceDB;Integrated Security=True";
+
+            using (var con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_Insert_Customer", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@FirstName", todoDTO.title);
+                cmd.Parameters.AddWithValue("@LastName", todoDTO.description);
+               
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+           
+        }
+    }
+}
---
+++ CustomerAPI/clsCustomer.cs
@@ -0,0 +1,8 @@
+namespace CustomerAPI
+{
+    public class clsCustomer
+    {
+        public string? title { get; set; }
+        public string? description { get; set; }
+    }
+}
